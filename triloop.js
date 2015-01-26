fcfe2218b51892eaef6fea64ccdfb1435696965a
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-01-26 12:11:48 -0500

    Forgot to put a comment into my code

--- triloop.js
+++ triloop.js
@@ -1,2 +1,6 @@
+// Jared Smith
+// Ch 2
+// Makes a triangle of hashtags
+// Last work was done on Saturday
 for (var hashtag = "#"; hashtag <= "#######"; hashtag = hashtag + "#")
 	console.log(hashtag);
