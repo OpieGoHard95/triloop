37435139903beb72ea6510680352119ac331c2ad
OpieGoHard95 <jsmith2@saintjoe.edu>
2015-02-13 14:29:24 -0500

    Reverted back to original state

--- triloop.js
+++ triloop.js
@@ -4,4 +4,4 @@
 // Last work was done on Saturday
 for (var hashtag = "#"; hashtag <= "#######"; hashtag = hashtag + "#")
 	console.log(hashtag);
-// Needed to alter somethng to test github, this will be deleted
+
